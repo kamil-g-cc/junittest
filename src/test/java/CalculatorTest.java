2a566d3e31972c758aaa2b3349e55d761fcfb6ad
Kamil Grondys <kgrondys@gmail.com>
2021-04-10 13:30:08 +0200

    adding test cases to add method

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    @Test
+    void add_TwoPlusTwo_expectFour(){
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.add(2,2));
+    }
+
+    @Test
+    void add_TwoPossitiveNumbers_expectingPositiveResult() {
+        Calculator calculator = new Calculator();
+        assertTrue(calculator.add(2,2)>0);
+    }
 }
