dec03af2c32a37ea1642c46d28f5f73771e83eab
Kamil Grondys <kgrondys@gmail.com>
2021-04-10 13:14:28 +0200

    adding App and Calculator classes

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello ;)");
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(2,2));
+    }
+}
