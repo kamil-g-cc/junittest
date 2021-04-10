dad61c0ac1f314a89423bf18afe6663545002480
Kamil Grondys <kgrondys@gmail.com>
2021-04-10 13:46:54 +0200

    adding beforeeach

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,30 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    private Calculator calculator;
+    @BeforeEach
+    void init(){
+        calculator = new Calculator();
+    }
 
     @Test
     void add_TwoPlusTwo_expectFour(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals(4, calculator.add(2,2));
     }
 
     @Test
     void add_TwoPossitiveNumbers_expectingPositiveResult() {
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertTrue(calculator.add(2,2)>0);
     }
 
     @Test
     void divide_DevideByZero_expectingInfinity(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertTrue(Double.isInfinite(calculator.divide(2, 0)));
 
     }
     @Test
     void divideUnsupportingInfinity_DevideByZero_expectingException() {
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertThrows(IllegalArgumentException.class, () -> {
             calculator.divideUnsupportingInfinity(2, 0);
         });
